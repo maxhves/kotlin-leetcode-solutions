cb39c5738f5a37ca4e0842815cbd2f81ff72fd0d
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2023-01-17 12:49:19 +0100

    Added a solution to problem 2535

---
+++ src/main/kotlin/easy/2535.kt
@@ -0,0 +1,46 @@
+package easy
+
+import java.util.Arrays
+
+fun main(args: Array<String>) {
+    differenceOfSum(intArrayOf(1, 15, 6, 3))
+}
+
+private fun differenceOfSum(nums: IntArray): Int {
+    println(nums.contentToString())
+    // Problem
+    // Positive integer array *nums*
+    // Sum of all elements in *nums*
+    // Sum of all the digits that appear in *nums*
+    // Return the difference between the two sums
+
+    // Case
+    // [1,15,6,3]
+    // Element sum is: 25
+    // Digit sum is: 16
+    // Return difference: (25-16) = 9
+
+    var elementSum: Int = 0
+    var digitString: String = ""
+
+    for (num in nums) {
+        elementSum += num
+
+        digitString += num
+    }
+
+    println(digitString)
+
+    val digitInts = digitString.map { it.toString().toInt() }
+    var digitSum: Int = 0
+
+    for (digit in digitInts) {
+        digitSum += digit
+    }
+
+    val difference = elementSum - digitSum
+
+    println(difference)
+
+    return difference
+}
