049d6da01ca6e1127e3c25ab7b8a80faf03d49c5
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2023-01-18 12:33:46 +0100

    Added solution for 2011

---
+++ src/main/kotlin/easy/2011.kt
@@ -0,0 +1,32 @@
+package easy
+
+fun main(args: Array<String>) {
+    finalValueAfterOperations(arrayOf("--X", "X++", "X++"))
+}
+
+private fun finalValueAfterOperations(operations: Array<String>): Int {
+    // Problem
+    // Programming language with only 4 operations and one variable X
+    // - ++X and X++ increments the value of the variable X by 1
+    // - --X and X-- decrements the value of the variable X by 1
+    // Initially the value of X is 0
+    // Take an array of strings *operations* containing a list of operations and return the final value of X after
+    // performing the given list of operations
+
+    println(operations.contentToString())
+
+    var x = 0
+
+    for (operation in operations) {
+        when (operation) {
+            "++X",
+            "X++" -> x++
+            "--X",
+            "X--" -> x--
+        }
+    }
+
+    println(x)
+
+    return x
+}
