e21e28a729cf07f1e73845e8d3ccc3e76eafb717
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2023-01-18 12:16:52 +0100

    Added solutions for 1689 and 2235

---
+++ src/main/kotlin/easy/1689.kt
@@ -0,0 +1,31 @@
+package easy
+
+fun main(args: Array<String>) {
+    minPartitions("32")
+}
+
+private fun minPartitions(n: String): Int {
+    // Problem
+    // A decimal number is a deci-binary if each of its digits are either 0 or 1 without leading zeros
+    // 101 and 1100 are deci-binary
+    // 112 and 3001 are not
+
+    // Given a string *n* that represents a positive decimal integer
+    // Return the minimum number of positive deci-binary numbers needed so that they sum up to *n*
+
+    // Case
+    // n = "32"
+    // 10 + 11 + 11 = 32
+    // Output = 3
+
+    // The minimum amount of deci-binary numbers needed is equal to the max digit
+
+    var max = 0
+
+    for (char in n) {
+        val digit = char.toString().toInt()
+        max = if (digit > max) digit else max
+    }
+
+    return max
+}
---
+++ src/main/kotlin/easy/2235.kt
@@ -0,0 +1,17 @@
+package easy
+
+fun main(args: Array<String>) {
+    sum(12, 5)
+    sum(-10, 4)
+}
+
+private fun sum(num1: Int, num2: Int): Int {
+    // Problem
+    // Given two integers, return the sum of the two integers
+    val sum = num1 + num2
+
+    println("Num1: $num1, Num2: $num2")
+    println(sum)
+
+    return sum
+}
