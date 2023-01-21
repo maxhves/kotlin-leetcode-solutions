52a36c65bf75a44b892ccdf8a3ae7ff813f0e66b
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2023-01-21 19:30:45 +0100

    Added solution for CountingSort1

---
+++ src/main/kotlin/hackerrank/CountingSort1.kt
@@ -0,0 +1,24 @@
+package hackerrank
+
+fun countingSort(arr: Array<Int>): Array<Int> {
+    // Problem
+    // Given a list of integers, count and return the number of times each value appears as an array of integers
+
+    val countedArray = IntArray(100)
+
+    for (i in arr) {
+        countedArray[i]++
+    }
+
+    return countedArray.toTypedArray()
+}
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.trim().toInt()
+
+    val arr = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    val result = countingSort(arr)
+
+    println(result.joinToString(" "))
+}
