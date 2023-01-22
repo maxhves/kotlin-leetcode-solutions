c34e12f1fb91d7dae2867eeb67f5289c3c5fc7f6
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2023-01-22 19:57:39 +0100

    Added solution for BalancedBrackets

---
+++ src/main/kotlin/hackerrank/BalancedBrackets.kt
@@ -0,0 +1,51 @@
+package hackerrank
+
+import java.util.*
+
+fun isBalanced(s: String): String {
+    // Problem
+    // Push into a queue
+    // Remove from queue when it's buddy is found
+    // If the list is empty then all is balanced?
+
+    // Case
+    // {[]()}
+    // {
+    // {[
+    // {
+    // {(
+    // {
+    val stack = Stack<Char>()
+
+    for (i in s) {
+        if (stack.isEmpty()) {
+            stack.push(i)
+        } else {
+            val head = stack.peek()
+
+            if (head == '{' && i == '}') {
+                stack.pop()
+            } else if (head == '(' && i == ')') {
+                stack.pop()
+            } else if (head == '[' && i == ']') {
+                stack.pop()
+            } else {
+                stack.push(i)
+            }
+        }
+    }
+
+    return if (stack.isEmpty()) "YES" else "NO"
+}
+
+fun main(args: Array<String>) {
+    val t = readLine()!!.trim().toInt()
+
+    for (tItr in 1..t) {
+        val s = readLine()!!
+
+        val result = isBalanced(s)
+
+        println(result)
+    }
+}
