a3d72b34b5f4011198f13fefbadde7609875d851
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2023-01-23 14:09:28 +0100

    Added solution for SimpleTextEditor

--- src/main/kotlin/hackerrank/SimpleTextEditor.kt
+++ src/main/kotlin/hackerrank/SimpleTextEditor.kt
@@ -1,22 +1,33 @@
 package hackerrank
 
+import java.util.*
+
 class Editor {
-    private val fullText: String = ""
+    private val textStack = Stack<String>()
 
     fun append(text: String) {
-
+        if (textStack.isEmpty()) {
+            textStack.push(text)
+        } else {
+            textStack.push(getCurrentText().plus(text))
+        }
     }
 
-    fun delete(number: Int) {
-
+    fun deleteLast(number: Int) {
+        textStack.push(getCurrentText().dropLast(number))
     }
 
-    fun getChar(position: Int) {
-
+    fun getChar(position: Int): Char {
+        val currentText = getCurrentText()
+        return currentText[(position - 1) % currentText.length]
     }
 
     fun undoLastOperation() {
+        textStack.pop()
+    }
 
+    private fun getCurrentText(): String {
+        return textStack.peek()
     }
 }
 
@@ -27,6 +38,25 @@ fun main(args: Array<String>) {
     // - Print the Kth character of S
     // - Undo the last (not previously undone) operation of type 1 or 2
     //   reverting S to the state it was in prior to that operation
+    val editor = Editor()
+    val numOfOperations = readLine()!!.trim().toInt()
 
+    for (lineCount in 1..numOfOperations) {
+        val line = readLine()!!.trim()
 
+        when (line[0]) {
+            '1' -> {
+                editor.append(line.removeRange(0, 1).trim())
+            }
+            '2' -> {
+                editor.deleteLast(line.removeRange(0, 1).trim().toInt())
+            }
+            '3' -> {
+                println(editor.getChar(line.removeRange(0, 1).trim().toInt()))
+            }
+            '4' -> {
+                editor.undoLastOperation()
+            }
+        }
+    }
 }
