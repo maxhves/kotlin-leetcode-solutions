51467bd22eb0bf632f206c7290e212553b5ac441
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2023-01-23 09:52:39 +0100

    Created SimpleTextEditor class

---
+++ src/main/kotlin/hackerrank/SimpleTextEditor.kt
@@ -0,0 +1,32 @@
+package hackerrank
+
+class Editor {
+    private val fullText: String = ""
+
+    fun append(text: String) {
+
+    }
+
+    fun delete(number: Int) {
+
+    }
+
+    fun getChar(position: Int) {
+
+    }
+
+    fun undoLastOperation() {
+
+    }
+}
+
+fun main(args: Array<String>) {
+    // 4 operations
+    // - Append string W to the end of S
+    // - Delete the last k character S
+    // - Print the Kth character of S
+    // - Undo the last (not previously undone) operation of type 1 or 2
+    //   reverting S to the state it was in prior to that operation
+
+
+}
