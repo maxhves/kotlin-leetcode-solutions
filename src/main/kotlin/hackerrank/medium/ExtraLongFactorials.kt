a8567a7885c0cf44d5ad7483bfdfc3b8fff67973
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2023-01-24 19:56:21 +0100

    Added solution for ExtraLongFactorials

---
+++ src/main/kotlin/hackerrank/medium/ExtraLongFactorials.kt
@@ -0,0 +1,19 @@
+package hackerrank.medium
+
+import java.math.BigInteger
+
+fun extraLongFactorials(n: Int): Unit {
+    var factorial: BigInteger = BigInteger.ONE
+
+    for (i in n downTo 1) {
+        factorial *= i.toBigInteger()
+    }
+
+    println(factorial)
+}
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.trim().toInt()
+
+    extraLongFactorials(n)
+}
