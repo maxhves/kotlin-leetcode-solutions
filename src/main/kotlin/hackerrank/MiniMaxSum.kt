3ae673079737395ba1333a32e3da5e6aa04e1408
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2023-01-24 19:00:40 +0100

    Added better solution to MiniMaxSum

--- src/main/kotlin/hackerrank/MiniMaxSum.kt
+++ src/main/kotlin/hackerrank/MiniMaxSum.kt
@@ -1,7 +1,9 @@
 package hackerrank
 
 import java.math.BigInteger
+import java.util.*
 
+// Initial
 fun miniMaxSum(arr: Array<Int>): Unit {
     // Problem
     // Given 5 positive integers;
@@ -23,9 +25,34 @@ fun miniMaxSum(arr: Array<Int>): Unit {
     // 2063136757 2744467344
 }
 
+// Better
+fun miniMaxSum2(arr: Array<Int>) {
+    var sum: Long = arr[0].toLong()
+    var min = arr[0]
+    var max = arr[0]
+
+    for (i in 1 until arr.size) {
+        val current = arr[i]
+
+        sum += current
+
+        // If min is bigger than the current item then it isn't the smallest
+        if (min > current) {
+            min = current
+        }
+
+        // If max is smaller than the current item then it isn't the biggest
+        if (max < current) {
+            max = current
+        }
+    }
+
+    println("${sum - max} ${sum - min}")
+}
+
 fun main(args: Array<String>) {
 
     val arr = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
 
-    miniMaxSum(arr)
+    miniMaxSum2(arr)
 }
