5d65d176bc3b326d7d71221316196e0dc6606674
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2023-01-25 14:17:02 +0100

    Added solution for AppleAndOrange

--- src/main/kotlin/hackerrank/easy/AppleAndOrange.kt
+++ src/main/kotlin/hackerrank/easy/AppleAndOrange.kt
@@ -1,7 +1,46 @@
 package hackerrank.easy
 
-fun countApplesAndOranges(s: Int, t: Int, a: Int, b: Int, apples: Array<Int>, oranges: Array<Int>): Unit {
-    
+fun countApplesAndOranges(
+    s: Int, // House start
+    t: Int, // House end
+    a: Int, // Apple tree
+    b: Int, // Orange tree
+    apples: Array<Int>, // Apples that fall
+    oranges: Array<Int>, // Oranges that fall
+): Unit {
+    // Apples: -n moves away from house, +n moves closer to house
+    // Oranges: -n moves closes to house, +n moves away from house
+
+    // Difference between apple tree and house start
+    // Difference between orange tree and house end
+
+    // aT: 1, hS: 3, hE: 4, oT: 6
+    // if apple is > 1 && < 4
+    // if orange is > -1 && < -4
+    // Then the fruit will touch the house
+
+    // Count the apples
+    var appleInHouseCount = 0
+
+    // Loop through the apples array
+    for (apple in apples) {
+        // Check the apples that are in the house
+        if (apple >= (s - a) && apple <= (t - a)) {
+            appleInHouseCount++
+        }
+    }
+
+    // Do the same for oranges
+    var orangeInHouseCount = 0
+
+    for (orange in oranges) {
+        if (orange <= (t - b) && orange >= (s - b)) {
+            orangeInHouseCount++
+        }
+    }
+
+    println(appleInHouseCount)
+    println(orangeInHouseCount)
 }
 
 fun main(args: Array<String>) {
