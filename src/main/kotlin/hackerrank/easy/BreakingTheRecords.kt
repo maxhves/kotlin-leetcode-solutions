d8eedaff16ff413f603286293625d33d13403ac1
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2023-01-25 19:31:21 +0100

    Added solution for BreakingTheRecords

---
+++ src/main/kotlin/hackerrank/easy/BreakingTheRecords.kt
@@ -0,0 +1,31 @@
+package hackerrank.easy
+
+fun breakingRecords(scores: Array<Int>): Array<Int> {
+    var currentMin = scores[0]
+    var currentMax = scores[0]
+    val maxMinCount = IntArray(2)
+
+    for (i in 1..scores.lastIndex) {
+        val score = scores[i]
+
+        if (score > currentMax) {
+            currentMax = score
+            maxMinCount[0]++
+        } else if (score < currentMin) {
+            currentMin = score
+            maxMinCount[1]++
+        }
+    }
+
+    return maxMinCount.toTypedArray()
+}
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.trim().toInt()
+
+    val scores = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    val result = breakingRecords(scores)
+
+    println(result.joinToString(" "))
+}
