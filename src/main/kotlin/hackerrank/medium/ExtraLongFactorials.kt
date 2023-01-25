4b23e1b92d1c916bcafd28b419127b577082b23e
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2023-01-25 08:07:02 +0100

    Replaced BigInteger.ONE with BigInteger.ZERO

--- src/main/kotlin/hackerrank/medium/ExtraLongFactorials.kt
+++ src/main/kotlin/hackerrank/medium/ExtraLongFactorials.kt
@@ -3,7 +3,7 @@ package hackerrank.medium
 import java.math.BigInteger
 
 fun extraLongFactorials(n: Int): Unit {
-    var factorial: BigInteger = BigInteger.ONE
+    var factorial: BigInteger = BigInteger.ZERO
 
     for (i in n downTo 1) {
         factorial *= i.toBigInteger()
