07c0b2258d613daf7dfdf2d88f561a701cc8fcd4
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2023-01-26 09:00:42 +0100

    Added solution for TheHurdleRace

---
+++ src/main/kotlin/hackerrank/easy/TheHurdleRace.kt
@@ -0,0 +1,20 @@
+package hackerrank.easy
+
+fun hurdleRace(k: Int, height: Array<Int>): Int {
+    val doses = height.max() - k
+    return if (doses < 0) 0 else doses
+}
+
+fun main(args: Array<String>) {
+    val first_multiple_input = readLine()!!.trimEnd().split(" ")
+
+    val n = first_multiple_input[0].toInt()
+
+    val k = first_multiple_input[1].toInt()
+
+    val height = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    val result = hurdleRace(k, height)
+
+    println(result)
+}
