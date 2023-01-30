a1fe9e48d6e22a56455e9fa9c75c353d79d15c11
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2023-01-30 06:49:21 +0100

    Added class for BetweenTwoSets

---
+++ src/main/kotlin/hackerrank/easy/BetweenTwoSets.kt
@@ -0,0 +1,21 @@
+package hackerrank.easy
+
+fun getTotalX(a: Array<Int>, b: Array<Int>): Int {
+
+}
+
+fun main(args: Array<String>) {
+    val first_multiple_input = readLine()!!.trimEnd().split(" ")
+
+    val n = first_multiple_input[0].toInt()
+
+    val m = first_multiple_input[1].toInt()
+
+    val arr = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    val brr = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    val total = getTotalX(arr, brr)
+
+    println(total)
+}
