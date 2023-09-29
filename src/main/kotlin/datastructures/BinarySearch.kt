494f78ab585200714c3acc69adc06a9db8c0925d
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2023-09-29 16:26:47 -0700

    Added BinarySearch class.

---
+++ src/main/kotlin/datastructures/BinarySearch.kt
@@ -0,0 +1,27 @@
+package datastructures
+
+import kotlin.math.floor
+
+private fun main() {
+    val result = binarySearch(list = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9), target = 8)
+    println(result)
+}
+
+fun binarySearch(list: IntArray, target: Int): Int? {
+    var first = 0
+    var last = list.lastIndex
+
+    while (first <= last) {
+        val midpoint: Int = (first + last) / 2
+
+        if (list[midpoint] == target) {
+            return midpoint
+        } else if (list[midpoint] < target) {
+            first = midpoint + 1
+        } else {
+            last = midpoint - 1
+        }
+    }
+
+    return null
+}
