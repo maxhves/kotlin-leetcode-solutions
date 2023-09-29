ea528e26b2d3a7605b9c856b35e7e77433a63342
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2023-09-28 19:38:48 -0700

    Added solution for 2652.

---
+++ src/main/kotlin/leetcode/easy/2652.kt
@@ -0,0 +1,47 @@
+package leetcode.easy
+
+//region Problem
+
+/**
+ * Given a positive integer `n`, find the sum of all integers in the range `[1, n]` inclusive
+ * that are divisible by `3`, `5`, or `7`.
+ *
+ * Return an integer denoting the sum of all numbers in the given range satisfying the constraint.
+ */
+
+//endregion
+
+//region Test cases
+
+/**
+ * Input: n = 7
+ * Output: 21
+ *
+ * Input: n = 10
+ * Output: 40
+ */
+
+//endregion
+
+//region Solution
+
+private fun main() {
+    sumOfMultiples(n = 7)
+    sumOfMultiples(n = 10)
+}
+
+fun sumOfMultiples(n: Int): Int {
+    val sum: Int = (1..n)
+        .fold(0) { acc, it ->
+            if (it % 3 == 0 || it % 5 == 0 || it % 7 == 0) {
+                acc + it
+            } else {
+                acc
+            }
+        }
+
+    println("Sum of multiples: $sum")
+    return sum
+}
+
+//endregion
