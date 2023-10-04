d2b9a4f84cf718a326850dc0d96e638efc5d8172
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2023-10-03 17:12:31 -0700

    Added. solution to 34.

---
+++ src/main/kotlin/leetcode/medium/34.kt
@@ -0,0 +1,58 @@
+package leetcode.medium
+
+//region Problem
+
+
+//endregion
+
+//region Test cases
+
+
+//endregion
+
+//region Solution
+
+private fun main() {
+
+}
+
+fun searchRange(nums: IntArray, target: Int): IntArray {
+    val answer = intArrayOf(-1, -1)
+
+    val start = getIndexOfTarget(nums, target, true)
+    val end = getIndexOfTarget(nums, target, false)
+
+    answer[0] = start
+    answer[1] = end
+
+    return answer
+}
+
+fun getIndexOfTarget(nums: IntArray, target: Int, findStartIndex: Boolean): Int {
+    var answer = -1
+    var start = 0
+    var end = nums.lastIndex
+
+    while (start <= end) {
+        val midpoint = start + (end - start) / 2
+
+        if (target < nums[midpoint]) {
+            end = midpoint - 1
+        } else if (target > nums[midpoint]) {
+            start = midpoint + 1
+        } else {
+            // Potential answer is found
+            answer = midpoint
+
+            if (findStartIndex) {
+                end = midpoint - 1
+            } else {
+                start = midpoint + 1
+            }
+        }
+    }
+
+    return answer
+}
+
+//endregion
