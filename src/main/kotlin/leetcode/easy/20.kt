755a4b332738793435c5343cb370e0458e2502cb
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2023-10-16 08:03:23 -0700

    Added solution to 20.

---
+++ src/main/kotlin/leetcode/easy/20.kt
@@ -0,0 +1,60 @@
+package leetcode.easy
+
+import java.util.*
+
+//region Problem
+
+
+
+//endregion
+
+//region Steps to solve
+
+
+
+//endregion
+
+//region Solution
+
+private fun main() {
+    // Output: true
+    isValid(s = "{[]}")
+
+    // Output: true
+    isValid(s = "()")
+
+    // Output: true
+    isValid(s = "()[]{}")
+
+    // Output: false
+    isValid(s = "(]")
+}
+
+private fun isValid(s: String): Boolean {
+    val stack = Stack<Char>()
+    val matchMap = mapOf(
+        ')' to '(',
+        '}' to '{',
+        ']' to '[',
+    )
+
+    for (char in s) {
+        if (stack.isEmpty()) {
+            stack.push(char)
+        } else {
+            if (matchMap.containsKey(char)) {
+                if (matchMap[char] == stack.peek()) {
+                    stack.pop()
+                } else {
+                    stack.push(char)
+                }
+            } else {
+                stack.push(char)
+            }
+        }
+    }
+
+    return stack.isEmpty()
+}
+
+//endregion
