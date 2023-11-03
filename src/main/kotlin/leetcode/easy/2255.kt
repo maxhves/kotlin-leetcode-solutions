f935ce6488a6a15e9bb4d754d62a0ab997015d75
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2023-11-03 07:23:34 -0700

    Added solution to 2255.

---
+++ src/main/kotlin/leetcode/easy/2255.kt
@@ -0,0 +1,61 @@
+package leetcode.easy
+
+//region Problem
+
+/**
+ * You are given a string array (words) and a string (s), where (words[i]) and (s) comprise only of
+ * lowercase English letters.
+ *
+ * Return the number of strings in (words) that are a prefix of (s).
+ *
+ * A prefix of a string is a substring that occurs at the beginning of the string. A substring is a
+ * contiguous sequence of characters within a string.
+ */
+
+//endregion
+
+//region Steps to solve
+
+/**
+ * Considerations:
+ *  - We need to know if (word) is a prefix of (s).
+ *  - Meaning; does (s) begin with (word)?
+ *
+ * 1. Initialize a set to track prefix instances.
+ * 2. Iterate over (s) and for each index:
+ *     - Add the accumulated prefix to our set.
+ * 3. Iterate over the (words) array.
+ * 4. For each (word);
+ *     - Check if the (word) exists in our set.
+ *     - If the (word) exists, it is a prefix, increase our match count.
+ * 5. Return the total match count.
+ */
+
+//endregion
+
+//region Solution
+
+private fun main() {
+
+}
+
+private fun countPrefixes(words: Array<String>, s: String): Int {
+    val possiblePrefixes = HashSet<String>()
+    var prefixMatchCount = 0
+
+    var accumulatedPrefix = ""
+    for (char in s) {
+        accumulatedPrefix += char
+        possiblePrefixes.add(accumulatedPrefix)
+    }
+
+    for (word in words) {
+        if (possiblePrefixes.contains(word)) {
+            prefixMatchCount++
+        }
+    }
+
+    return prefixMatchCount
+}
+
+//endregion
