d1e57694806010472fc881ec1df5cbcea8080da3
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2023-11-04 15:47:45 -0700

    Added solution to 981.

--- src/main/kotlin/leetcode/medium/981.kt
+++ src/main/kotlin/leetcode/medium/981.kt
@@ -22,8 +22,10 @@ package leetcode.medium
 /**
  * Considerations:
  *  - We should do a binary search based on the (timestamp) variable.
- *
- * 1.
+ *  - We need to store a key, with that contains a value at a given timestamp.
+ *  - Could this be a HashMap:
+ *     - Key: String.
+ *     - Value: List<Pair<String, Int>> (value to timestamp).
  */
 
 //endregion
@@ -31,7 +33,53 @@ package leetcode.medium
 //region Solution
 
 private fun main() {
+    val timeMap = TimeMap()
+    timeMap.set("foo", "bar", 1)
+    timeMap.get("foo", 1)
+    timeMap.get("foo", 3)
+}
 
+private class TimeMap() {
+    private val store = HashMap<String, MutableList<Pair<String, Int>>>()
+
+    fun set(key: String, value: String, timestamp: Int) {
+        store.getOrPut(key) { mutableListOf() }.add(value to timestamp)
+    }
+
+    fun get(key: String, timestamp: Int): String {
+        if (!store.containsKey(key)) {
+            return ""
+        } else {
+            val pairs = store[key]!!
+            val match = binarySearch(pairs, timestamp)
+
+            if (match < 0) {
+                return ""
+            } else {
+                return pairs[match].first
+            }
+        }
+    }
+
+    private fun binarySearch(pairs: List<Pair<String, Int>>, target: Int): Int {
+        var left = 0
+        var right = pairs.lastIndex
+
+        while (left <= right) {
+            val midpoint = (left + right) / 2
+            val midpointPair = pairs[midpoint]
+
+            if (target > midpointPair.second) {
+                left = midpoint + 1
+            } else if (target < midpointPair.second) {
+                right = midpoint - 1
+            } else {
+                return midpoint
+            }
+        }
+
+        return right
+    }
 }
 
 //endregion
