55e2ecb2f822eda0ffe3ed8b2e4f04b82a73bae6
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2023-12-17 17:06:24 -0800

    Added solution to 212.

--- src/main/kotlin/leetcode/hard/212.kt
+++ src/main/kotlin/leetcode/hard/212.kt
@@ -16,7 +16,13 @@ package leetcode.hard
 
 /**
  * Steps
- * -
+ * - Using a Trie structure we can add each word into said structure.
+ * - We should then iterate over each element on the board:
+ *   - We should run a recursive function that will check if the element exists in our Trie.
+ *   - We then need to check the next position in all directions (apart from diagonally).
+ *   - If we come across a char that doesn't exist we can immediately return false for this function.
+ *   - If we reach a node that is a word, then we can add the current word to our result array.
+ * - Ultimately, return the resulting words we have found.
  */
 
 //endregion
@@ -37,21 +43,81 @@ private fun main() {
 
 private class TrieNode {
     val children = HashMap<Char, TrieNode>(26)
+    var isWord: Boolean = false
 }
 
-private fun findWords(board: Array<CharArray>, words: Array<String>): List<String> {
-    val trie = TrieNode()
+private fun findWords(
+    board: Array<CharArray>,
+    words: Array<String>,
+): List<String> {
     val rows = board.size
     val columns = board[0].size
+    val visited = Array(rows) { BooleanArray(columns) }
+    val trie = TrieNode()
+
+    val possibleWords = HashSet<String>()
+
+    for (word in words) {
+        var currentNode = trie
+
+        for (char in word) {
+            if (char !in currentNode.children) {
+                currentNode.children[char] = TrieNode()
+            }
+            currentNode = currentNode.children.getOrDefault(char, TrieNode())
+        }
+
+        currentNode.isWord = true
+    }
+
+    fun dfs(
+        row: Int,
+        column: Int,
+        currentNode: TrieNode,
+        currentWord: String,
+    ): Boolean {
+        if (currentNode.isWord) {
+            possibleWords.add(currentWord)
+        }
+
+        if (
+            row < 0 ||
+            column < 0 ||
+            row >= rows ||
+            column >= columns ||
+            visited[row][column]
+        ) {
+            return false
+        }
+
+        val char = board[row][column]
+
+        if (char !in currentNode.children) {
+            return false
+        }
+
+        visited[row][column] = true
+
+        val nextNode = currentNode.children.getOrDefault(char, TrieNode())
+        val found = (
+            dfs(row, column - 1, nextNode, "${currentWord}$char") ||
+            dfs(row, column + 1, nextNode, "${currentWord}$char") ||
+            dfs(row - 1, column, nextNode, "${currentWord}$char") ||
+            dfs(row + 1, column, nextNode, "${currentWord}$char")
+        )
+
+        visited[row][column] = false
+
+        return found
+    }
 
     for (row in 0 until rows) {
         for (column in 0 until columns) {
-            val centerItem = board[row][column]
-            trie.children[centerItem] = TrieNode()
+            dfs(row, column, trie, "")
         }
     }
 
-    return listOf()
+    return possibleWords.toList()
 }
 
 //endregion
